8cc8f0f0a8d7fdbbf6d102ad8b6c1765ddc590df
Kim <kimkim910108@gmail.com>
2026-04-20 00:24:43 +0800

    Initial Commit

---
+++ Worshipprojection/WorshipprojectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorshipprojectionApp.swift
+//  Worshipprojection
+//
+//  Created by Kim on 2026/4/20.
+//
+
+import SwiftUI
+
+@main
+struct WorshipprojectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
